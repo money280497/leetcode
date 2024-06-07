2b32c68524f2a7ac6dbb18b71bbf5272061aa882
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-06-07 20:08:21 +0530

    Time: 70 ms (53.5%), Space: 52.5 MB (90.14%) - LeetHub

---
+++ 0046-permutations/0046-permutations.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums, arr = [], res = []) {
+    if (nums.length === 0) return res.push([...arr]);
+    for (let i = 0; i < nums.length; i++) {
+        let rest = nums.filter((n, index) => index !== i);
+        arr.push(nums[i])
+        permute(rest, arr, res)
+        arr.pop()
+    }
+    return res
+};

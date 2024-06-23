67488b7262e736befaa6c2cf8a73966583a4801b
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-06-23 12:50:09 +0530

    Time: 65 ms (13.09%), Space: 49 MB (64.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -5,7 +5,6 @@
  */
 var uniquePaths = function (m, n) {
     const dp = Array.from(Array(m), () => new Array(n).fill(1));
-    console.log({ dp })
     for (let i = 1; i < dp.length; i++) {
         for (let j = 1; j < dp[0].length; j++) {
             dp[i][j] = dp[i - 1][j] + dp[i][j - 1]

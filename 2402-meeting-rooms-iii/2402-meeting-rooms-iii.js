04d48149682a3ce11e9a326c6d50a92779280575
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-06-27 19:17:00 +0530

    Time: 278 ms (68.29%), Space: 82.6 MB (60.16%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.js
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.js
@@ -4,24 +4,35 @@
  * @return {number}
  */
 var mostBooked = function (n, meetings) {
-    meetings.sort((a, b) => a[0] - b[0]);
-    const roomTime = new Array(n).fill(0);
-    const countRoomFreq = new Array(n).fill(0);
-    for (let [startTime, endTime] of meetings) {
-        let freeRoomIndex = roomTime.findIndex(endTime => endTime <= startTime);
-        if (freeRoomIndex < 0) {
-            freeRoomIndex = roomTime.indexOf(Math.min(...roomTime));
-        }
-        countRoomFreq[freeRoomIndex]++;
-        let lastMeetingEndtime = roomTime[freeRoomIndex];
-        let meetingEndTime;
-        if (startTime < roomTime[freeRoomIndex]) {
-            meetingEndTime = (lastMeetingEndtime - startTime) + endTime;
+
+    const sortedMeetings = meetings.sort((a, b) => a[0] - b[0])
+
+    const roomsEndTimer = new Array(n).fill(0);
+    const roomsOccupancyTime = new Array(n).fill(0);
+
+    for (let i = 0; i < sortedMeetings.length; i++) {
+        const emptyRoomIndex = roomsEndTimer.findIndex((element) => element <= sortedMeetings[i][0])
+        if (emptyRoomIndex != -1) {
+            roomsEndTimer[emptyRoomIndex] = sortedMeetings[i][1]
+            roomsOccupancyTime[emptyRoomIndex]++
         }
         else {
-            meetingEndTime = endTime;
+            const roomToBeEmpty = Math.min(...roomsEndTimer)
+            const roomToBeEmptyIndex = roomsEndTimer.indexOf(roomToBeEmpty)
+
+            //increase room end timer
+            roomsEndTimer[roomToBeEmptyIndex] = roomsEndTimer[roomToBeEmptyIndex] >= sortedMeetings[i][0] ? endCalculator(roomsEndTimer, roomToBeEmptyIndex, sortedMeetings, i) : sortedMeetings[i][1]
+
+            //increase meetings held in room
+            roomsOccupancyTime[roomToBeEmptyIndex]++;
         }
-        roomTime[freeRoomIndex] = meetingEndTime
     }
-    return countRoomFreq.indexOf(Math.max(...countRoomFreq));
+
+    const roomWithMaxMeetings = Math.max(...roomsOccupancyTime)
+    const indexOfRoomWithMaxMeetings = roomsOccupancyTime.indexOf(roomWithMaxMeetings)
+    return indexOfRoomWithMaxMeetings
 };
+
+const endCalculator = (roomsEndTimer, roomToBeEmptyIndex, sortedMeetings, i) => {
+    return roomsEndTimer[roomToBeEmptyIndex] - sortedMeetings[i][0] + sortedMeetings[i][1]
+}

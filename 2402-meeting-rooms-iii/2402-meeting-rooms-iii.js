06cdf842243887489e2b470b5320fbbb43140905
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-06-27 10:53:48 +0530

    Time: 249 ms (82.93%), Space: 80.9 MB (87.8%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @param {number[][]} meetings
+ * @return {number}
+ */
+var mostBooked = function (n, meetings) {
+    meetings.sort((a, b) => a[0] - b[0]);
+    const roomTime = new Array(n).fill(0);
+    const countRoomFreq = new Array(n).fill(0);
+    for (let [startTime, endTime] of meetings) {
+        let freeRoomIndex = roomTime.findIndex(endTime => endTime <= startTime);
+        if (freeRoomIndex < 0) {
+            freeRoomIndex = roomTime.indexOf(Math.min(...roomTime));
+        }
+        countRoomFreq[freeRoomIndex]++;
+        let lastMeetingEndtime = roomTime[freeRoomIndex];
+        let meetingEndTime;
+        if (startTime < roomTime[freeRoomIndex]) {
+            meetingEndTime = (lastMeetingEndtime - startTime) + endTime;
+        }
+        else {
+            meetingEndTime = endTime;
+        }
+        roomTime[freeRoomIndex] = meetingEndTime
+    }
+    return countRoomFreq.indexOf(Math.max(...countRoomFreq));
+};

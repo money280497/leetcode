d1449536770de33a02cc4aa615ffc05eba3858ae
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-07-09 09:11:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var findMatrix = function (nums) {
+    const result = []
+    const cache = {};
+    for (const item of nums) {
+        cache[item] = (cache[item] || 0) + 1;
+    }
+    while (Object.keys(cache).length) {
+        const res = [];
+        let i = 0;
+        for (let i of Object.keys(cache)) {
+            if (cache[i] === 0) delete cache[i]
+            else {
+                res.push(parseInt(i))
+                cache[i] -= 1
+            }
+        }
+        if (res.length > 0) {
+            result.push(res)
+        }
+    }
+    return res
+};

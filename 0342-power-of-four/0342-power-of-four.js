6961b3a9e2344a84c9f77a60f78439e2547c0363
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-07-25 13:33:27 +0530

    Time: 66 ms (81.62%), Space: 52.8 MB (26.35%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function (n) {
+    if (n === 0) return false
+    if (n === 1) {
+        return true
+    }
+    if (n % 4 !== 0) {
+        return false
+    }
+    return isPowerOfFour(Math.floor(n / 4))
+};

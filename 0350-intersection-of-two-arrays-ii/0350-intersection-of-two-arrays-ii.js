645adc3914d7c1fb8b90846322ce0ee90c98c068
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-07-28 13:51:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -3,23 +3,37 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+// var intersect = function (nums1, nums2) {
+//     const map1 = new Map();
+//     const result = [];
+//     for (let i = 0; i < nums1.length; i++) {
+//         map1.set(nums1[i], (map1.get(nums1[i]) || 0) + 1)
+//     }
+//     for (let i = 0; i < nums2.length; i++) {
+//         map2.set(nums2[i], (map2.get(nums2[i]) || 0) + 1)
+//     }
+//     for (let [key, value] of map1) {
+//         if (map2.has(key)) {
+//             let start = 0
+//             while (start < Math.min(value, map2.get(key))) {
+//                 result.push(key);
+//                 start++
+//             }
+//         }
+//     }
+//     return result;
+// };
+
 var intersect = function (nums1, nums2) {
     const map1 = new Map();
-    const map2 = new Map();
     const result = [];
     for (let i = 0; i < nums1.length; i++) {
         map1.set(nums1[i], (map1.get(nums1[i]) || 0) + 1)
     }
     for (let i = 0; i < nums2.length; i++) {
-        map2.set(nums2[i], (map2.get(nums2[i]) || 0) + 1)
-    }
-    for (let [key, value] of map1) {
-        if (map2.has(key)) {
-            let start = 0
-            while (start < Math.min(value, map2.get(key))) {
-                result.push(key);
-                start++
-            }
+        if (map1.has(nums2[i]) && map1.get(nums2[i]) > 0) {
+            map1[nums2[i]]--;
+            result.push(nums2[i])
         }
     }
     return result;

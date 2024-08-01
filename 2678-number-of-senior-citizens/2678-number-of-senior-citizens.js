091376051cf22b8291f780fb46f319c68fc99cf9
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-08-01 14:28:44 +0530

    Time: 50 ms (88.54%), Space: 50.1 MB (96.66%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.js
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.js
@@ -2,25 +2,25 @@
  * @param {string[]} details
  * @return {number}
  */
-// var countSeniors = function (details) {
-//     // let count = 0;
-//     // for (let person of details) {
-//     //     if (parseInt(person.charAt(11) + person.charAt(12)) > 60)
-//     //         count++
-//     // }
-//     // return count
-//     return details.reduce((acc, cur) => {
-//         if (parseInt(cur.substring(11, 13)) > 60)
-//             return acc++;
-//         return acc;
-//     }, 0)
-// };
-
 var countSeniors = function (details) {
+    // let count = 0;
+    // for (let person of details) {
+    //     if (parseInt(person.charAt(11) + person.charAt(12)) > 60)
+    //         count++
+    // }
+    // return count
     return details.reduce((acc, cur) => {
-        if (parseInt(cur.substring(11, 13)) > 60) {
-            acc++;
-        }
+        if (parseInt(cur.substring(11, 13)) > 60)
+            return ++acc;
         return acc;
-    }, 0);
+    }, 0)
 };
+
+// var countSeniors = function (details) {
+//     return details.reduce((acc, cur) => {
+//         if (parseInt(cur.substring(11, 13)) > 60) {
+//             acc++;
+//         }
+//         return acc;
+//     }, 0);
+// };

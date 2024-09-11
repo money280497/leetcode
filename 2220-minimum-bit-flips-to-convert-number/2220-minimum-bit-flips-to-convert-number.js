f29ef371fcfb6ce09fd9abf6ae7e93106699b9e0
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-09-11 07:25:37 +0530

    Time: 56 ms (44.2%), Space: 48.3 MB (94.93%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} start
+ * @param {number} goal
+ * @return {number}
+ */
+var minBitFlips = function (start, goal) {
+    return (start ^ goal).toString(2).split("0").join("").length;
+};

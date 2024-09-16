6ec7abfefe0da0b2d71272835a3fd1fe7006dd54
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-09-16 10:34:34 +0530

    Time: 73 ms (68.16%), Space: 54.7 MB (27.61%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let i = 0, j = 0;
+    while (i < nums.length) {
+        if (nums[i] !== 0) {
+            nums[j] = nums[i];
+            j++;
+        }
+        i++;
+    }
+    while (j < nums.length) {
+        nums[j] = 0;
+        j++;
+    }
+    return nums
+};

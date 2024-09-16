54436375d5d18a72564cae9efb4f81b53c54c64d
Mohit Mehta <60932938+money280497@users.noreply.github.com>
2024-09-16 15:32:22 +0530

    Time: 192 ms (8.87%), Space: 74.8 MB (5.52%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -4,22 +4,20 @@
  * @return {number}
  */
 
- const maxOperations = (nums, k) => {
-  let count = new Map();
-  let operations = 0;
-
-  for (const num of nums) {
-    let complement = k - num;
-
-    if (count.has(complement) && count.get(complement) > 0) {
-      operations += 1;
-      count.set(complement, count.get(complement) - 1);
-    } else {
-      count.set(num, (count.get(num) || 0) + 1);
+var maxOperations = function (nums, k) {
+    let obj1 = {}, count = 0;
+    for (let item of nums) {
+        obj1[item] ? obj1[item] = obj1[item] + 1 : obj1[item] = 1;
     }
-  }
-
-  return operations;
+    for (let item of nums) {
+        if (obj1[k - item] && obj1[k - item] > 0) {
+            obj1[k - item] -= 1;
+            obj1[item] -= 1;
+            if (obj1[item] > -1 && obj1[k - item] > -1)
+                count++;
+        }
+    }
+    return count
 };
 // var maxOperations = function (nums, k) {
 //     let obj1 = {}, count = 0;
